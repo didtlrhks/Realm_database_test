4800b4416f0f018f03bc676bf08abcd5891e80d5
sikwan <yysskk99999@naver.com>
2024-04-02 11:18:12 +0900

    init

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Realm_database_test
+//
+//  Created by 양시관 on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Realm_database_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_database_testApp.swift
+//  Realm_database_test
+//
+//  Created by 양시관 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Realm_database_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
